8f0a1abf4b0d0cdac1a204a380a514fcc639d936
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-17 08:58:06 +0200

    chore: bump version to 1.1.2

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,6 @@
 return {
     name        = "puissance4",
-    version     = "1.1.1",
+    version     = "1.1.2",
     fullname    = "Puissance 4",
     description = "Connect Four — alignez 4 pièces avec IA.",
 }
