1cdf441d763df492c827419d3fc11eb5f7edd59c
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 15:05:04 +0200

    chore: bump version to 1.1.14

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,6 @@
 local _ = require("gettext")
 return {
-    version     = "1.1.13",
+    version     = "1.1.14",
     fullname    = _("Connect Four"),
     description = _("Connect Four — align 4 pieces with AI."),
 }
